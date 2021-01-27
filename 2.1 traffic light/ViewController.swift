b52b9fff5e241687edebf3791dfa84a51b96ddf2
Elena <aalazuka@gmail.com>
2021-01-27 19:50:59 +0300

    Initial Commit

---
+++ 2.1 traffic light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.1 traffic light
+//
+//  Created by Елена Лазука on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
